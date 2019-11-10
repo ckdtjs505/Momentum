7c3ad6d38c1604445827f9642d6ac124bf8d06bb
ckdtjs505 <ckdtjs505@naver.com>
2019-11-10 11:45:25 +0900

    [CREATE] todolist 삽입 삭제 기능 구현

--- todo.js
+++ todo.js
@@ -1,20 +1,60 @@
 const todoinput = todo.querySelector('input'),
     list = document.querySelector('.js-list');
 
-function handleTodoSubmit (e){
-    e.preventDefault();
-    console.log();
+let datalist = [];
 
+function saveDatalist(datalist){
+    localStorage.setItem('currentList', JSON.stringify(datalist));
+}
+
+function handleClick(e){
+    const parentButton = e.target.parentNode;
+    // console.log(e.target.previousSibling.data);
+    datalist = datalist.filter(function(data){
+        return data.data != e.target.previousSibling.data;
+    })
+    saveDatalist(datalist);
+    parentButton.remove();
+}
+
+function createlist(value){
     const li = document.createElement('li');
     const btn = document.createElement('button');
-    li.innerHTML = todoinput.value;
+    li.innerHTML = value;
     btn.innerHTML = 'x';
-    todoinput.value = '';
     list.appendChild(li);
     li.appendChild(btn);
+    btn.addEventListener('click', handleClick);
+}
+
+function handleTodoSubmit (e){
+    e.preventDefault();
+    createlist(todoinput.value)
+    const addList = {
+        data : todoinput.value
+    }
+    datalist.push(addList);
+    saveDatalist(datalist);
+    todoinput.value = '';
+}
+
+function paintingList(currentList){
+    currentList.forEach(function(data){
+        createlist(data.data);
+    })
+
+    datalist = currentList;
+}
+
+function getList(){
+    const currentList = localStorage.getItem('currentList');
+    if(currentList !== null){
+        paintingList(JSON.parse(currentList));
+    }
 }
 
 function init(){
+    getList();
     todo.addEventListener('submit', handleTodoSubmit) 
 }
 init();
