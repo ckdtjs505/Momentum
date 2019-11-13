6f725e713722e7c704480b5ac8cb1e770cd72c7c
ckdtjs505 <ckdtjs505@naver.com>
2019-11-14 07:01:01 +0900

    [UPDATE] name.js user 이름 변경 기능 이벤트 구현

--- js/name.js
+++ js/name.js
@@ -13,15 +13,29 @@ function handleSubmit(e) {
     saveUser();
 }
 
+function userClickHandle(e) {
+    const userContext = document.createElement('strong');
+    console.dir(userContext)
+}
+
 function painting(user) {
     const div = document.createElement('div');
     const hour = (new Date).getHours();
+    const userContext = document.createElement('strong');
     div.innerHTML = `${(hour >+ 18) ? `Good evening` : 
     (hour <18 && hour >+ 12) ? `Good afternoon` :
-    (hour < 12) ? `Good Morning` : '' }, ${user}.`;
+    (hour < 12) ? `Good Morning` : '' }, `;
+    userContext.innerHTML = `${user}.`
+
+   
+    userContext.className = 'userName';
     name.className = 'hiding';
     todo.classList.remove('hiding');
+    div.appendChild(userContext);
     output.appendChild(div);
+
+
+    userContext.addEventListener('click', userClickHandle)
 }
 
 function getCurrentUser() {
