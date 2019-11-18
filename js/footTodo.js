9ff56a6abeb9ebb985d4c2298bd377cdd9e90154
ckdtjs505 <ckdtjs505@naver.com>
2019-11-18 14:12:11 +0900

    [CREATE] footTodo.js의 footTodo close  Button 동작 구현

--- js/footTodo.js
+++ js/footTodo.js
@@ -1,16 +1,21 @@
 const footTodoButton = document.querySelector('#js-todoButton'), 
     settings = document.querySelector('#js-setting');
-
+    closeFootTodoButton = document.querySelector('.js-closeFootTodoButton');
 function handelfootTodoButton(e) {
     const footTodolist = document.querySelector('.js-footTodolist');
     footTodolist.classList.remove('hiding');
-    console.log(footTodolist)
+    
+    closeFootTodoButton.addEventListener('click', function HandlecloseFootTodoButton(e) {
+        e.preventDefault();
+        footTodolist.classList.add('hiding');      
+    })
 }
 
 function handelsettings(e) {
     alert('setting Todo');
 }
 function init() {
+    console.log('object')
     footTodoButton.addEventListener('click', handelfootTodoButton);
     settings.addEventListener('click', handelsettings);
 }
