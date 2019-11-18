2ec0d5d3b2c54b133f165847e7b695c2c5a73f3d
ckdtjs505 <ckdtjs505@naver.com>
2019-11-18 23:52:26 +0900

    [UPDATE] footTodo.js dom 동작 완성

--- js/footTodo.js
+++ js/footTodo.js
@@ -2,6 +2,8 @@ const footTodoButton = document.querySelector('#js-todoButton'),
     settings = document.querySelector('#js-setting');
 
 function handelfootTodoButton(e) {
+    const currentFootData = localStorage.getItem('currentFootData');
+     
     const closeFootTodoButton = document.querySelector('.js-closeFootTodoButton');
     const footTodolist = document.querySelector('.js-footTodolist');
     const askTodo = document.querySelector('#js-askTodo');
@@ -10,31 +12,62 @@ function handelfootTodoButton(e) {
 
     footTodolist.classList.remove('hiding');
 
-    askTodoButton.addEventListener('click', function HandleAsktodoButton(e) {
-        e.preventDefault();
-        // askTodo.classList.add('hiding');
-        inputTodo.classList.remove('hiding');
-    })
-
     closeFootTodoButton.addEventListener('click', function HandlecloseFootTodoButton(e) {
         e.preventDefault();
         inputTodo.value = '';
         inputTodo.classList.add('hiding');
         footTodolist.classList.add('hiding');      
     })
-   
+
+    if(currentFootData !== null){
+        
+        askTodo.classList.add('hiding');
+        inputTodo.classList.remove('hiding');
+
+        let todolist = document.querySelector('#js-todolist');
+        const currentlistData = localStorage.getItem('currentFootData');
+        const datali = document.createElement('li'); 
+        datali.innerText = currentlistData;
+        todolist.appendChild(datali);
+        todolist.classList.remove('hiding');
+    }else {
+        
+    
+        inputTodo.parentElement.addEventListener('submit', function HandleinputTodo(e) {
+            e.preventDefault();
+            askTodo.classList.add('hiding');
+            localStorage.setItem('currentFootData', inputTodo.value);
+            inputTodo.value = '';
+
+            
+            // painting 
+            let todolist = document.querySelector('#js-todolist');
+            const currentlistData = localStorage.getItem('currentFootData');
+            const datali = document.createElement('li'); 
+            datali.innerText = currentlistData;
+            todolist.appendChild(datali);
+            todolist.classList.remove('hiding');
+            
+            
+        })
+    
+        askTodoButton.addEventListener('click', function HandleAsktodoButton(e) {
+            e.preventDefault();
+            inputTodo.classList.remove('hiding');
+        })
+
+        
+    }
 }
 
 function handelsettings(e) {
     alert('setting Todo');
 }
+
 function init() {
-    
 
     footTodoButton.addEventListener('click', handelfootTodoButton);
     settings.addEventListener('click', handelsettings);
-
-   
 }
 
 init();
