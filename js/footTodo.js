be04da0ff7719187cea0c8db02a54711e9526eb9
ckdtjs505 <ckdtjs505@naver.com>
2019-11-18 14:54:20 +0900

    [UPDATE] footTodo.js Newtodo 동작 구현

--- js/footTodo.js
+++ js/footTodo.js
@@ -1,23 +1,40 @@
 const footTodoButton = document.querySelector('#js-todoButton'), 
     settings = document.querySelector('#js-setting');
-    closeFootTodoButton = document.querySelector('.js-closeFootTodoButton');
+
 function handelfootTodoButton(e) {
+    const closeFootTodoButton = document.querySelector('.js-closeFootTodoButton');
     const footTodolist = document.querySelector('.js-footTodolist');
+    const askTodo = document.querySelector('#js-askTodo');
+    const askTodoButton = askTodo.querySelector('button');
+    const inputTodo = document.querySelector('.js-inputTodo');
+
     footTodolist.classList.remove('hiding');
-    
+
+    askTodoButton.addEventListener('click', function HandleAsktodoButton(e) {
+        e.preventDefault();
+        // askTodo.classList.add('hiding');
+        inputTodo.classList.remove('hiding');
+    })
+
     closeFootTodoButton.addEventListener('click', function HandlecloseFootTodoButton(e) {
         e.preventDefault();
+        inputTodo.value = '';
+        inputTodo.classList.add('hiding');
         footTodolist.classList.add('hiding');      
     })
+   
 }
 
 function handelsettings(e) {
     alert('setting Todo');
 }
 function init() {
-    console.log('object')
+    
+
     footTodoButton.addEventListener('click', handelfootTodoButton);
     settings.addEventListener('click', handelsettings);
+
+   
 }
 
 init();
