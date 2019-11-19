b55f4c1f0ebcdff43bfdd34de962ab36cbc13e06
ckdtjs505 <ckdtjs505@naver.com>
2019-11-19 12:52:54 +0900

    [REFACTORING] footTodo.js inputTodoList 코드 정리

--- js/footTodo.js
+++ js/footTodo.js
@@ -1,97 +1,104 @@
 const footTodoButton = document.querySelector('#js-todoButton'), 
-    settings = document.querySelector('#js-setting');
-
+    settings = document.querySelector('#js-setting'),
+    inputTodo = document.querySelector('.js-inputTodo'),
+    askTodo = document.querySelector('#js-askTodo');
 let currentFootTodolist = [];
+
+function HandleinputTodo(e) {
+    e.preventDefault();
+    askTodo.classList.add('hiding');
+    const data = {
+        data : inputTodo.value
+    }
+    
+    inputTodo.value = ''; 
+    if(data.data !== ''){
+        currentFootTodolist.push(data);
+        localStorage.setItem('currentFootData', JSON.stringify(currentFootTodolist));
+    }
+
+    //painting
+    paintingList(currentFootTodolist);
+}
+
+function paintingList(Data) {
+    let todolist = document.querySelector('#js-todolist');
+    todolist.querySelectorAll('*').forEach( n => n.remove());
+    
+    Data.forEach(function(data) {
+        const datali = document.createElement('li'); 
+        datali.innerText = data.data;
+        todolist.appendChild(datali);
+    }) 
+    todolist.classList.remove('hiding');
+}
+
 function handelfootTodoButton(e) {
     const currentFootData = localStorage.getItem('currentFootData');
-     
     const closeFootTodoButton = document.querySelector('.js-closeFootTodoButton');
     const footTodolist = document.querySelector('.js-footTodolist');
-    const askTodo = document.querySelector('#js-askTodo');
     const askTodoButton = askTodo.querySelector('button');
-    const inputTodo = document.querySelector('.js-inputTodo');
-
-    footTodolist.classList.remove('hiding');
-
-    closeFootTodoButton.addEventListener('click', function HandlecloseFootTodoButton(e) {
-        e.preventDefault();
-        inputTodo.value = '';
-        inputTodo.classList.add('hiding');
-        footTodolist.classList.add('hiding');      
-    })
-
+    
+    // 데이터가 있을 시     
     if(currentFootData !== null){
-        
+        // 데이터가 있으므로 물어보는 html 숨기기
         askTodo.classList.add('hiding');
-        inputTodo.classList.remove('hiding');
 
-        let todolist = document.querySelector('#js-todolist');
-        todolist.querySelectorAll('*').forEach( n => n.remove());
-        const currentlistData = JSON.parse(localStorage.getItem('currentFootData'));
+        // todolist html 등장
+        footTodolist.classList.remove('hiding'); 
+        inputTodo.classList.remove('hiding');
         
+        // js-tododolist dom 초기화
+        let todolist = document.querySelector('#js-todolist');
+        todolist.querySelectorAll('*').forEach( n => n.remove()); // 기존의 데이터 삭제
+
+        // currentlistData 데이터 가져오기
+        const currentlistData = JSON.parse(localStorage.getItem('currentFootData')); 
+        currentFootTodolist = currentlistData;
+
+        // currentlistData 데이터 그리기
         currentlistData.forEach(function(data) {
             const datali = document.createElement('li'); 
             datali.innerText = data.data;
             todolist.appendChild(datali);
         }) 
-        todolist.classList.remove('hiding');
 
-        inputTodo.parentElement.addEventListener('submit', function HandleinputTodo(e) {
+        // todolist datalist를 html 보이기 
+        todolist.classList.remove('hiding');
+        
+        // inputTodo data input event
+        inputTodo.parentElement.addEventListener('submit', HandleinputTodo );
+
+        closeFootTodoButton.addEventListener('click', function HandlecloseFootTodoButton(e) {
             e.preventDefault();
-            askTodo.classList.add('hiding');
-            
-            const data = {
-                data : inputTodo.value
-            }
-            
-            inputTodo.value = ''; 
-            if(data.data !== ''){
-                currentFootTodolist.push(data);
-                localStorage.setItem('currentFootData', JSON.stringify(currentFootTodolist));
-            }
-            //painting
-            let todolist = document.querySelector('#js-todolist');
-            todolist.querySelectorAll('*').forEach( n => n.remove());
-            const currentlistData = JSON.parse(localStorage.getItem('currentFootData'));
-            
-            currentlistData.forEach(function(data) {
-                const datali = document.createElement('li'); 
-                datali.innerText = data.data;
-                todolist.appendChild(datali);
-            }) 
-            todolist.classList.remove('hiding');
+            inputTodo.value = '';
+            inputTodo.classList.add('hiding');
+            footTodolist.classList.add('hiding');      
         })
+
+       
 
     }else {
-        inputTodo.parentElement.addEventListener('submit', function HandleinputTodo(e) {
-            e.preventDefault();
-            askTodo.classList.add('hiding');
-            
-            const data = {
-                data : inputTodo.value
-            }
-            
-            inputTodo.value = ''; 
-            currentFootTodolist.push(data);
-            localStorage.setItem('currentFootData', JSON.stringify(currentFootTodolist));
-            
-            //painting
-            let todolist = document.querySelector('#js-todolist');
-            todolist.querySelectorAll('*').forEach( n => n.remove());
-            const currentlistData = JSON.parse(localStorage.getItem('currentFootData'));
-            
-            currentlistData.forEach(function(data) {
-                const datali = document.createElement('li'); 
-                datali.innerText = data.data;
-                todolist.appendChild(datali);
-            }) 
-            todolist.classList.remove('hiding');
-            
-        })
-    
+        // 데이터 없을 시 
+
+        // todolist html 등장
+        footTodolist.classList.remove('hiding'); 
+
+        // inputTodo data input event
+        inputTodo.parentElement.addEventListener('submit', HandleinputTodo);
+        
+        // askTodoButton event
         askTodoButton.addEventListener('click', function HandleAsktodoButton(e) {
             e.preventDefault();
             inputTodo.classList.remove('hiding');
+        })
+
+         // askTodoButton event
+        closeFootTodoButton.addEventListener('click', function HandlecloseFootTodoButton(e) {
+            e.preventDefault();
+            inputTodo.value = '';
+            inputTodo.classList.add('hiding');
+            footTodolist.classList.add('hiding');      
         })
     }
 }
